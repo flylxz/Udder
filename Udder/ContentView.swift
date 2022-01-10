e8ed484269e4ad1cca54c3d85dd4f11c9b797c7a
Alexandr Gladchenko <front.end.aam.dezncode@gmail.com>
2022-01-10 16:50:18 +0200

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Александр Гладченко on 1/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Александр Гладченко on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
